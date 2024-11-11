f91152d38c96492bd03128dd6af45cd00c1f2a14
Xavier Mitault <xavier.mitault@epitech.eu>
2024-11-11 16:30:26 +0000

    Fix install g++

--- client/scripts/install-deps-linux.sh
+++ client/scripts/install-deps-linux.sh
@@ -5,7 +5,7 @@ if command -v apt-get 2>/dev/null; then
     apt-get update
     apt-get install -y g++ cmake clang-tidy clang-format xorg-dev
 elif command -v pacman 2>/dev/null; then
-    pacman -Sy g++ cmake clang
+    pacman -Sy gcc cmake clang
 elif command -v dnf 2>/dev/null; then
     dnf install -y libXcursor-devel libXi-devel libXinerama-devel libXrandr-devel cmake clang-tools-extra gcc-c++ make cmake
 else
