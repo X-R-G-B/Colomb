79afecb21aa8f05a3839ef2b84403fdad25fcda8
Xavier Mitault <xavier.mitault@epitech.eu>
2024-11-11 17:47:22 +0000

    Format src

--- client/src/firstmenu/FirstMenu.cpp
+++ client/src/firstmenu/FirstMenu.cpp
@@ -1,12 +1,11 @@
 #include "FirstMenu.hpp"
 #include "PathResolver.hpp"
 
-FirstMenu::FirstMenu(raylib::Window &/*unused*/)
-    : _icon(PathResolver::resolve("assets/icons/favicon.png"))
+FirstMenu::FirstMenu(raylib::Window & /*unused*/) : _icon(PathResolver::resolve("assets/icons/favicon.png"))
 {
 }
 
-void FirstMenu::update(raylib::Window &/*unused*/)
+void FirstMenu::update(raylib::Window & /*unused*/)
 {
 }
 
@@ -17,6 +16,6 @@ void FirstMenu::draw(raylib::Window &window)
         window.GetHeight() / 2 - FirstMenu::_icon.GetHeight() / 2);
 }
 
-void FirstMenu::free(raylib::Window &/*unused*/)
+void FirstMenu::free(raylib::Window & /*unused*/)
 {
 }
