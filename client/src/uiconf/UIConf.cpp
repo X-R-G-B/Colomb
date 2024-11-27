17654e6fc40c825ade101f93aca0b235eac509c7
Xavier Mitault <xavier.mitault@epitech.eu>
2024-11-27 02:49:30 +0000

    Add file download

--- client/src/uiconf/UIConf.cpp
+++ client/src/uiconf/UIConf.cpp
@@ -1,8 +1,12 @@
+#include <fstream>
 #include <memory>
 #include <optional>
 #include <stdexcept>
 #include "INetwork.hpp"
 #include "UIConf.hpp"
+#include "Logger.hpp"
+#include "PathResolver.hpp"
+#include "httplib.h"
 
 template<typename T>
 std::optional<T> json_get(const nlohmann::json &json, const std::string &key);
@@ -591,4 +595,21 @@ const std::string &UIConf::UIPopUp::getId() const
 
 UIConf::Asset::Asset(const std::string &identifier, const std::string &urlPath): _identifier(identifier), _urlPath(urlPath)
 {
+    std::string host(_urlPath);
+    host.erase(host.find('/', host.find("://") + 3));
+    std::string path(_urlPath.substr(host.size()));
+
+    Logger::info("UICONF_ASSET: Downloading host=" + host + "; path=" + path + ";");
+    httplib::Client cli(host);
+
+    cli.set_follow_location(true);
+    const auto &res = cli.Get(path);
+    if (res->status != 200) {
+        throw std::runtime_error("This should not happend: failed to load assets");
+    }
+    const auto filePath = PathResolver::resolve(identifier);
+    std::ofstream file(filePath);
+    file << res->body;
+    file.close();
+    _path = filePath;
 }
